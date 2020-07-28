42983fc6077858fdd1890d2c6050c533cd5275cd
Asheer Rizvi <asheer.vil@gmail.com>
2020-07-28 18:38:21 +0530

    Updates

---
+++ precache-manifest.acc102532f0be0d578d0771ede56c827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41df1664c4ab5e99a5cb5325fdee799",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "218b43b2137afbf12ee8",
+    "url": "/tic-tac-toe/static/css/main.cdf6c684.chunk.css"
+  },
+  {
+    "revision": "7f357ec36ca21f93a8ce",
+    "url": "/tic-tac-toe/static/js/2.cecc969b.chunk.js"
+  },
+  {
+    "revision": "b74735d7b836a5b8c2053f269ef2aa60",
+    "url": "/tic-tac-toe/static/js/2.cecc969b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218b43b2137afbf12ee8",
+    "url": "/tic-tac-toe/static/js/main.efd5025c.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
